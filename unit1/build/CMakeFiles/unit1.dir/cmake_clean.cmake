13b43e40b25b8f605c06240e79adb013adeb137c
marcus.zou <marcus.zou@gmail.com>
2024-10-01 12:39:37 +0200

    day 4

---
+++ unit1/build/CMakeFiles/unit1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit1.dir/main.cpp.o"
+  "CMakeFiles/unit1.dir/main.cpp.o.d"
+  "unit1"
+  "unit1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ unit3/build/CMakeFiles/unit3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit3.dir/main.cpp.o"
+  "CMakeFiles/unit3.dir/main.cpp.o.d"
+  "unit3"
+  "unit3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
